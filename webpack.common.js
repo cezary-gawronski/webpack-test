d4a1900345ef5e5b4b9582996bc428293bace9a8
cezary-gawronski <idetepowiec@gmail.com>
2020-06-01 16:42:10 +0200

    added file: cezary.html

--- webpack.common.js
+++ webpack.common.js
@@ -63,5 +63,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/cezary.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "cezary.html",
+    }),
   ],
 };
